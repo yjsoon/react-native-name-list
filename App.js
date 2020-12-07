f23e25050e09a315ba751bdcaa1d125fa401cafe
YJ Soon <yjsoon@tinkertanker.com>
2020-12-07 10:46:38 +0800

    Imperative way of showing a lot of names

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const names = ["Alex", "Bob", "Charlie", "Derrick"];
+const names = [
+  "Aaran",
+  "Aaren",
+  "Aarez",
+  "Aarman",
+  "Aaron",
+  "Aaron-James",
+  "Aarron",
+  "Aaryan",
+  "Aaryn",
+  "Aayan",
+  "Aazaan",
+  "Abaan",
+  "Abbas",
+  "Abdallah",
+  "Abdalroof",
+  "Abdihakim",
+  "Abdirahman",
+  "Abdisalam",
+  "Abdul",
+  "Abdul-Aziz",
+  "Abdulbasir",
+  "Abdulkadir",
+  "Abdulkarem",
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>{names[0]}</Text>
-      <Text>{names[1]}</Text>
-      <Text>{names[2]}</Text>
-      <Text>{names[3]}</Text>
-    </View>
-  );
+  function displayNames() {
+    let namesJSX = [];
+
+    for (let i = 0; i < names.length; i++) {
+      namesJSX.push(<Text>{names[i]}</Text>);
+    }
+
+    return namesJSX;
+  }
+
+  return <View style={styles.container}>{displayNames()}</View>;
 }
 
 const styles = StyleSheet.create({
